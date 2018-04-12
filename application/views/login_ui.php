0b9b0b5fc0207645478face1a68298086b5b0055
Jerry Cheung <SmallOyster@users.noreply.github.com>
2018-04-12 22:09:54 +0800

    #4 update get site url method
    ~ 更新获取网站根目录的方法

--- application/views/login_ui.php
+++ application/views/login_ui.php
@@ -9,11 +9,11 @@
 		<title>BCS商赛系统-用户登录</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
-		<!--[if lte IE 8]><script src="<?php echo $base_url; ?>/assets/js/html5shiv.js"></script><![endif]-->
-		<link rel="stylesheet" href="<?php echo $base_url; ?>/assets/css/main.css" />
-		<!--[if lte IE 9]><link rel="stylesheet" href="<?php echo $base_url; ?>/assets/css/ie9.css" /><![endif]-->
-		<!--[if lte IE 8]><link rel="stylesheet" href="<?php echo $base_url; ?>/assets/css/ie8.css" /><![endif]-->
-		<noscript><link rel="stylesheet" href="<?php echo $base_url; ?>/assets/css/noscript.css" /></noscript>
+		<!--[if lte IE 8]><script src="<?php echo site_url(); ?>/assets/js/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="<?php echo site_url(); ?>/assets/css/main.css" />
+		<!--[if lte IE 9]><link rel="stylesheet" href="<?php echo site_url(); ?>/assets/css/ie9.css" /><![endif]-->
+		<!--[if lte IE 8]><link rel="stylesheet" href="<?php echo site_url(); ?>/assets/css/ie8.css" /><![endif]-->
+		<noscript><link rel="stylesheet" href="<?php echo site_url(); ?>/assets/css/noscript.css" /></noscript>
 	</head>
 	<body class="is-loading">
 
@@ -23,7 +23,7 @@
 				<!-- Main -->
 					<section id="main">
 						<header>
-							<span class="avatar"><img src="<?php echo $base_url; ?>/assets/img/avatar.jpg" alt="" /></span>
+							<span class="avatar"><img src="<?php echo site_url(); ?>/assets/img/avatar.jpg" alt="" /></span>
 							<h1>用户登录</h1>
 							<p>BCS商赛系统用户中心</p>
 						</header>
@@ -54,16 +54,16 @@
 			</div>
 
 		<!-- Scripts -->
-			<!--[if lte IE 8]><script src="<?php echo $base_url; ?>/assets/js/respond.min.js"></script><![endif]-->
+			<!--[if lte IE 8]><script src="<?php echo site_url(); ?>/assets/js/respond.min.js"></script><![endif]-->
 			<script>
 				if ('addEventListener' in window) {
 					window.addEventListener('load', function() { document.body.className = document.body.className.replace(/\bis-loading\b/, ''); });
 					document.body.className += (navigator.userAgent.match(/(MSIE|rv:11\.0)/) ? ' is-ie' : '');
 				}
 			</script>
-			<script type="text/javascript" src="<?php echo $base_url; ?>/assets/js/jquery-1.9.0.js"></script>
-			<script type="text/javascript" src="<?php echo $base_url; ?>/assets/js/common.js"></script>
-			<script type="text/javascript" src="<?php echo $base_url; ?>/assets/js/login.js"></script>
+			<script type="text/javascript" src="<?php echo site_url(); ?>/assets/js/jquery-1.9.0.js"></script>
+			<script type="text/javascript" src="<?php echo site_url(); ?>/assets/js/common.js"></script>
+			<script type="text/javascript" src="<?php echo site_url(); ?>/assets/js/login.js"></script>
 			<!-- Onekey Login Support -->
 			<script type="text/javascript" language="javascript" charset="UTF-8">
 				document.onkeydown = function(event) {
